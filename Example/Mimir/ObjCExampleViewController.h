12184536c3f3eefdf415802e2322ce9f9151d20f
Amer Eid <amereid92@gmail.com>
2020-09-21 00:23:30 +0300

    add objective c and swift usage example code

---
+++ Example/Mimir/ObjCExampleViewController.h
@@ -0,0 +1,14 @@
+//
+//  ObjCExampleViewController.h
+//  Mimir_Example
+//
+//  Created by Amer Eid on 9/20/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ObjCExampleViewController : UIViewController
+
+@end
+
